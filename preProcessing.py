43fe3fcdd823a8b8ce97786b9d81fd2b7bb27235
GOLVET Théo <golvet@mbp13golvet.home>
2019-12-16 00:02:52 +0100

    Modif preProcessing (ecriture dans csv)

--- preProcessing.py
+++ preProcessing.py
@@ -11,11 +11,13 @@ def preProcessing(FolderName):
     jazz_xlab = os.listdir(FolderName)# FolderName is a string which is called 'jazz_xlab'
     ExtraBeatChord = []
     preSequence = []
-    ChordSequence = []  
-    
+    ChordSequence = []
+    csvFile = open(FolderName+'../preprocessed_data.csv', 'w')
+
     for MusicPiece in jazz_xlab :
+        print(MusicPiece)
         # read every .xlab file and split the content in line
-        file = open('jazz_xlab/'+str(MusicPiece), 'r')
+        file = open(FolderName+MusicPiece, 'r')
         Content = file.read()
         file.close()
         Content = Content.split('\n')
@@ -26,33 +28,30 @@ def preProcessing(FolderName):
             CurrentBeat = int((CurrentLine[2]))
             CurrentChord = CurrentLine[5]
             ExtraBeatChord.append([CurrentBeat,CurrentChord])
-        
+
         Tonality = CurrentLine[::-1][0];
-        
+
         # write the Chords in a list respect their beat number
         for j in range(len(ExtraBeatChord)):
             for k in range(ExtraBeatChord[j][0]):
                 preSequence.append(ExtraBeatChord[j][1])
-        
+
         #map(+str(Tonality),preSequence)
 
-        # re-write the chord sequence as 16 chords in every line      
+        # re-write the chord sequence as 16 chords in every line
         for i in range(len(preSequence)):
             if i+16 <= len(preSequence):
                 temp = preSequence[i:i+16]
                 for j in range(16) :
                     if (j%4 + 1 +i%4)%4 == 0:
-                        temp[j] = temp[j] +str(':') + str(4)+str(':')+str(Tonality)
+                        temp[j] = temp[j] +str('%') + str(4)+str('%')+str(Tonality)
                     else:
-                        temp[j] = temp[j] +str(':') + str(((j%4 + 1 +i%4))%4)+str(':')+str(Tonality)
-                    
-                ChordSequence.append(temp)
-    return ChordSequence
+                        temp[j] = temp[j] +str('%') + str(((j%4 + 1 +i%4))%4)+str('%')+str(Tonality)
 
-ChordSequence = preProcessing('try_xlab')
-print(ChordSequence)
+                for k in range(15):
+                    csvFile.write(temp[k]+';')
+                csvFile.write(temp[15] + '\n')
+    csvFile.close()
 
-
-
-
-
+#ChordSequence = preProcessing('try_xlab')
+#print(ChordSequence)

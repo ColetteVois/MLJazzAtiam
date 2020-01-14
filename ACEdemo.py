6adc75cc4d629500674c2d7abacff6404cab96b0
unknown <gael-gao@example.com>
2020-01-14 15:59:26 +0100

    'update_16:00_14012020'

--- ACEdemo.py
+++ ACEdemo.py
@@ -1,15 +1,20 @@
-from ChordsAlphabets import *
-from ChordsToChromaVectors import *
+import ChordsAlphabets as CA
+import ChordsToChromaVectors as CTCV
 import numpy as np
 
-def ACEdemo(y_true,y_pred,OurAlphabet):
-    
-    #y_ture = np.asarray(mir_label_to_bin_chroma_vec(chord1_ture,mode = 'bin'))
-    #y_pred = np.asarray(mir_label_to_bin_chroma_vec(chord2_pred,mode = 'bin'))
+def deleteDuplicatedElementFromList(listA):
+        #return list(set(listA))
+        return sorted(set(listA), key = listA.index)
+
+
+def ACEdemo(chord1_ture,chord2_pred,OurAlphabet):
 
     Notes = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B']
-    #OurAlphabet = ['maj', 'min']         
-
+    
+    OurAlphabet = list(OurAlphabet.values())
+    delete_list = ['X']
+    OurAlphabet = [x for x in OurAlphabet if x not in delete_list]     
+    OurAlphabet = deleteDuplicatedElementFromList(OurAlphabet)
     list_label = []
     for i in Notes:
         for j in OurAlphabet:
@@ -17,7 +22,15 @@ def ACEdemo(y_true,y_pred,OurAlphabet):
                 list_label.append(i+str(':')+j)
             
     list_label.append('N')
-    Vector_list = list_mir_label_to_list_vec(list_label,mode='bin_chroma',chroma_mode='bin')
+    #print(list_label)
+    
+    y_true = np.zeros(len(list_label))
+    y_pred = np.zeros(len(list_label))
+    
+    y_true[list_label.index(chord1_ture)] = 1
+    y_pred[list_label.index(chord2_pred)] = 1
+    
+    Vector_list = CTCV.list_mir_label_to_list_vec(list_label,mode='bin_chroma',chroma_mode='bin')
 
     M =np.zeros((len(Vector_list),len(Vector_list)))
     k = 0.1
@@ -25,12 +38,17 @@ def ACEdemo(y_true,y_pred,OurAlphabet):
 
     for i in range(len(Vector_list)):
         for j in range(len(Vector_list)):
+            #print(Vector_list[i])
+            #print(Vector_list[j])
             M[i][j] = 1/(k + np.linalg.norm(np.asarray(Vector_list[i])-np.asarray(Vector_list[j])))
         
         M_bar =M/np.linalg.norm(M, ord = np.inf)
     
-    y_true_bar = y_true*M_bar
+    #print(y_true)
+    y_true_bar = y_true.dot(M_bar)
     result = np.linalg.norm(y_true_bar-y_pred)
+    #print(y_true_bar)
+    #print(y_pred)
     
     return result
 
@@ -39,7 +57,10 @@ def ACEdemo(y_true,y_pred,OurAlphabet):
 #%%
     
 # try to use
-y_true = [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0]
-y_pred = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0]
-Score = ACEdemo(y_true, y_pred,['maj', 'min', 'N'])
-print(Score)
+
+Score1 = ACEdemo('C:maj', 'E:maj',CA.a5)
+Score2 = ACEdemo('C:maj', 'C:min',CA.a5)
+Score3 = ACEdemo('C:maj', 'D:dim7',CA.a5)
+Score4 = ACEdemo('C:maj', 'C:maj',CA.a5)
+print(Score1,Score2, Score3, Score4)
+
